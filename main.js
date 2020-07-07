d3244cc206e226a71a1b45e7eea1300ad6b1ae8e
Sharod Bailey <Sharod.Bailey@codeimmersives.comy>
2020-07-07 12:19:05 -0400

    done

--- main.js
+++ main.js
@@ -1,6 +1,77 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const onlyOdds = (nums) => {
+  let result = [];
+  for (const num of nums) {
+    if (num % 2 === 1) {
+result.push(num)
+    }
+
+  }
+  return result;
+}
+onlyOdds([1, 2, 3, 4])
+
+const onlyEvens = (nums) => {
+  let result = [];
+  for (const num of nums) {
+    if (num % 2 === 0) {
+      result.push(num)
+
+    }
+
+  }
+return result
+}
+onlyEvens([1, 2, 3, 4])
+
+const shortNamesOnly = (names) => {
+  let result = [];
+  for (const name of names) {
+    if (name.length < 7) {
+      result.push(name)
+
+    }
+
+  }
+return result
+}
+shortNamesOnly([
+  'Colin',
+  'Mesuara',
+  'Chase',
+  'Dallas',
+  'Crystal',
+  'Marc',
+  'Miley',
+  'Sharod',
+])
+
+const dNames = (names) => {
+  let result = [];
+  for (name of names) {
+    if (name.includes('D')) {
+      result.push(name)
+
+    }
+
+  }
+
+return result;
+}
+dNames([
+  'Colin',
+  'Mesuara',
+  'Chase',
+  'Dallas',
+  'Crystal',
+  'Marc',
+  'Miley',
+  'Sharod',
+])
+
+
 
 
 
